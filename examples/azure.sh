38336930536d1ff10323c3930f736ddb6785fc31
Ryan Faircloth <ryan@dss-i.com>
2022-07-18 12:46:22 -0400

    feat: MVC for azure "minimal viable cluster"

---
+++ examples/azure.sh
@@ -0,0 +1,8 @@
+#/bin/env bash
+touch .values.yaml
+helm upgrade --install --namespace humio \
+    -f examples/humio-azure-nvme-$1.yaml \
+    -f examples/kafka-azure-$1.yaml \
+    -f examples/common.yaml \
+    -f .values.yaml \
+    small chart/
