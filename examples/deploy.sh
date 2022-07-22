b7ac9b403f76ca22baafbc5d23b006003c61588a
Ryan Faircloth <ryan@dss-i.com>
2022-07-22 09:10:48 -0400

    fix: helm instance deploy script rename

--- examples/deploy.sh
+++ examples/deploy.sh
@@ -1,8 +1,8 @@
 #/bin/env bash
 touch .values.yaml
 helm upgrade --install --namespace humio \
-    -f examples/humio-azure-nvme-$1.yaml \
-    -f examples/kafka-azure-$1.yaml \
+    -f examples/humio-$1-nvme-$2.yaml \
+    -f examples/kafka-$1-$2.yaml \
     -f examples/common.yaml \
     -f .values.yaml \
-    $1 chart/
+    $2 chart/
